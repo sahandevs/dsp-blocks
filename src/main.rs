f08dd16868c419a6024632d64da1049a9f0df7e7
Sahand <sahandevs@gmail.com>
2024-08-12 11:58:48 +0330

    add raylib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init().size(640, 480).title("DSP Blocks").build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::BLACK);
+        d.draw_text("Hello, world!", 12, 12, 20, Color::WHITE);
+    }
 }
